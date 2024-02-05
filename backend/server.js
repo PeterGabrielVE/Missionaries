5d62578452a721f0f380c9e0d329d1d25c83807b
Pedro Gabriel <gaboleal123@gmail.com>
2024-02-04 20:14:47 -0400

    first version api

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,127 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require("mysql");
+const server = express();
+server.use(bodyParser.json());
 
-const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
+//Establish the database connection
 
-app.use(cors(corsOptions));
+const db = mysql.createConnection({
 
-// parse requests of content-type - application/json
-app.use(express.json());
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "dbsmschool",
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to missionaries application." });
 });
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+db.connect(function (error) {
+    if (error) {
+      console.log("Error Connecting to DB");
+    } else {
+      console.log("successfully Connected to DB");
+    }
+  });
+
+//Establish the Port
+
+  server.listen(8085,function check(error) {
+    if (error) 
+    {
+    console.log("Error....dddd!!!!");
+    }
+
+    else 
+    {
+        console.log("Started....!!!! 8085");
+
+    }
 });
+
+//Create the Records
+
+server.post("/api/student/add", (req, res) => {
+    let details = {
+      stname: req.body.stname,
+      course: req.body.course,
+      fee: req.body.fee,
+    };
+    let sql = "INSERT INTO student SET ?";
+    db.query(sql, details, (error) => {
+      if (error) {
+        res.send({ status: false, message: "Student created Failed" });
+      } else {
+        res.send({ status: true, message: "Student created successfully" });
+      }
+    });
+  });
+
+
+
+//view the Records
+
+server.get("/api/student", (req, res) => {
+    var sql = "SELECT * FROM student";
+    db.query(sql, function (error, result) {
+      if (error) {
+        console.log("Error Connecting to DB");
+      } else {
+        res.send({ status: true, data: result });
+      }
+    });
+  });
+
+
+//Search the Records
+
+server.get("/api/student/:id", (req, res) => {
+    var studentid = req.params.id;
+    var sql = "SELECT * FROM student WHERE id=" + studentid;
+    db.query(sql, function (error, result) {
+      if (error) {
+        console.log("Error Connecting to DB");
+      } else {
+        res.send({ status: true, data: result });
+      }
+    });
+  });
+
+
+
+//Update the Records
+
+server.put("/api/student/update/:id", (req, res) => {
+    let sql =
+      "UPDATE student SET stname='" +
+      req.body.stname +
+      "', course='" +
+      req.body.course +
+      "',fee='" +
+      req.body.fee +
+      "'  WHERE id=" +
+      req.params.id;
+  
+    let a = db.query(sql, (error, result) => {
+      if (error) {
+        res.send({ status: false, message: "Student Updated Failed" });
+      } else {
+        res.send({ status: true, message: "Student Updated successfully" });
+      }
+    });
+  });
+
+
+
+  //Delete the Records
+
+  server.delete("/api/student/delete/:id", (req, res) => {
+    let sql = "DELETE FROM student WHERE id=" + req.params.id + "";
+    let query = db.query(sql, (error) => {
+      if (error) {
+        res.send({ status: false, message: "Student Deleted Failed" });
+      } else {
+        res.send({ status: true, message: "Student Deleted successfully" });
+      }
+    });
+  });
