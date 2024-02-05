cab5c59cc137e0dc3c0657c2341f1d493fd3ab29
Pedro Gabriel <gaboleal123@gmail.com>
2024-02-04 20:50:18 -0400

    front first version

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Student from './Student/Student';
+
+function App() {
+ 
+  return (
+    <>
+      <div className="App">
+        <Student/>
+      </div>
+    </>
+  )
+}
+
+export default App
